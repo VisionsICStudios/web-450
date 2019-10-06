f470b5f00a576c14f093cd0fac7b58db7379317c
Aaron R Wilson <43349938+VisionsICStudios@users.noreply.github.com>
2019-10-06 00:27:50 -0400

    Update app-routing.module.ts

--- week-3/wilson-node-quiz/src/app/app-routing.module.ts
+++ week-3/wilson-node-quiz/src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
+
+/*
+============================================
+; Title:  app-routing.module.ts (Week 3)
+; Author: Professor Krasso
+; Modified by: Aaron Wilson
+; Date: 2 Oct 2019
+; Description: MEAN Node Quiz.
+;===========================================
+*/
+
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
