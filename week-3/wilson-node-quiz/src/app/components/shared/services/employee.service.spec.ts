e1af39678a3307e282af06cc9a32bd22767ea877
Aaron R Wilson <43349938+VisionsICStudios@users.noreply.github.com>
2019-10-06 02:26:10 -0400

    Update employee.service.spec.ts

--- week-3/wilson-node-quiz/src/app/components/shared/services/employee.service.spec.ts
+++ week-3/wilson-node-quiz/src/app/components/shared/services/employee.service.spec.ts
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  employee.service.spec.ts (Week 3)
+; Author: Professor Krasso
+; Modified by: Aaron Wilson
+; Date: 2 Oct 2019
+; Description: MEAN Node Quiz.
+;===========================================
+*/
+
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeeService } from './employee.service';
