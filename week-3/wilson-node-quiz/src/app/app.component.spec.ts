f0fc19d760f2a5ab8f6094a66e96dbbdba3a821f
Aaron R Wilson <43349938+VisionsICStudios@users.noreply.github.com>
2019-10-06 00:59:52 -0400

    Update app.component.spec.ts

--- week-3/wilson-node-quiz/src/app/app.component.spec.ts
+++ week-3/wilson-node-quiz/src/app/app.component.spec.ts
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  app.component.spec.ts (Week 3)
+; Author: Professor Krasso
+; Modified by: Aaron Wilson
+; Date: 2 Oct 2019
+; Description: MEAN Node Quiz.
+;===========================================
+*/
+
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
